233b94c145e1c21aac62295cfaaac1e063491570
Yago <yagodev10@gmail.com>
2025-06-05 11:17:08 -0300

    first commit

---
+++ src/main/java/med/voll/api/MedController/MedicoController.java
@@ -0,0 +1,21 @@
+package med.voll.api.MedController;
+
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.medico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody DadosCadastroMedico dados ) {
+        repository.save(new medico(dados));
+    }
+}
